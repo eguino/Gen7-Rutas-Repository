d6a7c0cb47b08029d1d13f99f15de1dc44ce5606
Israel Eguino <eguinoiribeisrael@gmail.com>
2024-06-26 09:43:12 -0600

    Se implementa el metodo getById en choferesService

--- src/main/java/com/eguino/app/rutas/services/ChoferesService.java
+++ src/main/java/com/eguino/app/rutas/services/ChoferesService.java
@@ -27,7 +27,11 @@ public class ChoferesService implements IService<Chofer> {
 
     @Override
     public Optional<Chofer> getById(Long id) {
-        return Optional.empty();
+        try{
+            return Optional.ofNullable(choferesRepo.getById(id));
+        } catch (SQLException e){
+            throw new RuntimeException(e.getMessage(),e.getCause());
+        }
     }
 
     @Override
